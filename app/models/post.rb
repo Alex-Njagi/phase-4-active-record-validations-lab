8928638dc4d615b7f02a19931354fcb065161f86
Alex Njagi <1992anjagi@gmail.com>
2023-06-21 06:02:57 +0000

    Completed assignment?

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    #validate :clickbait_title
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+    #private
+    #def clickbait_title
+        #clickbait_phrases = ["Won't Believe", "Secret", /^Top \d+$/, "Guess"]
+        #unless clickbait_phrases.any? { |phrase| phrase.match?(:title) }
+            #errors.add(:title, "Not clickbait-y enough")
+        #end
+    #end
 end
